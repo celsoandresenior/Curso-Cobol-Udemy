b658d5205c14e18e39ec2488107e0b9118341c4a
Celso Lima <celsolog2012@gmail.com>
2022-11-27 11:10:06 -0300

    alterando formato do ano

--- Aula23.cob
+++ Aula23.cob
@@ -4,11 +4,13 @@
        file                                                    section .
        working-storage                                         section .
        77 ws-mostra pic x(20) value spaces. 
+       77 ws-data pic x(20) value spaces. 
        procedure                                              division .
        main-procedure.
            display "infome algo: ".
            accept ws-mostra
-           display ws-mostra.
+           accept ws-data from date YYYYMMDD                             Formatando data com ano em 4 digitos.
+           display ws-mostra ' - ' ws-data .
            stop run.
        end program Aula23.
        
