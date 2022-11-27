992971f818f933ffa57cd8184803149abf6b3b16
Celso Lima <celsolog2012@gmail.com>
2022-11-27 11:48:44 -0300

    entendendo comando set

---
+++ Aula25.cob
@@ -0,0 +1,21 @@
+       identification                                         division .
+       program-id. Aula25.
+       data                                                   division .
+       file                                                    section .
+       working-storage                                         section .
+       77 ws-num1      pic 99 value zeros.
+       77 ws-num2      pic 99 value zeros. 
+       
+       procedure                                              division .
+       main-procedure.
+           
+           set ws-num1 to 1.
+
+           display ws-num1
+
+           stop run.
+       end program Aula25.
+       
+       
+       
+       
