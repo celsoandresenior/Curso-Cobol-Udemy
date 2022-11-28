c608d2c7e04ea67c71066a2b35fdfa391f3c6a23
Celso Lima <celsolog2012@gmail.com>
2022-11-28 09:13:28 -0300

    solicitando valores ao usuario para fazer equacoes

--- aula27.cob
+++ aula27.cob
@@ -9,6 +9,8 @@
       * Area dedicada a declaração de variaveis. 
        working-storage                                         section .
        77 ws-resultado  pic 9(5) value zeros .
+       77 ws-num-1      pic 9(2) value zeros .
+       77 ws-num-2      pic 9(2) value zeros .
       *-----------------------------------------------------------------
       * Area dedicada a execução das procedures do programa.
       * basicamente area de processamento.
@@ -17,14 +19,23 @@
            display "******************INICIO DO PROCESSAMENTO**********"
            display "PROCEDURE PRINCIPAL...............................".
            perform 0001-inicio.
+           perform 0002-processamento.
            perform 0002-final.
  
-
        0001-inicio.
+           display "SOLICITANDO VALORES AO USUARIO....................".
+           display " ".
+           display "POR FAVOR INFORMAR O PRIMEIRO NUMERO:.............".
+           accept ws-num-1.
+           display "POR FAVOR INFORMAR O SEGUNDO  NUMERO:.............".
+           accept ws-num-2.
+           display " ".
+       0002-processamento.
            display "PROCESSAMENTO.....................................".
-           compute ws-resultado = 5 + 5.
+           compute ws-resultado = ws-num-1 + ws-num-2.
            compute ws-resultado = ws-resultado * ws-resultado.
            display "resultado.: " ws-resultado.
+           display " ".
     
        0002-final.   
            display "FINAL DO PROCESSAMENTO............................".
