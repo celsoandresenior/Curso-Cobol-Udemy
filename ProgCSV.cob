4f4ba3c8fdd7698a6c68637ee987e1915c1242e8
Celso Lima <celsolog2012@gmail.com>
2022-12-05 21:37:43 -0300

    corrigindo programa

--- ProgCSV.cob
+++ ProgCSV.cob
@@ -7,7 +7,7 @@
        IDENTIFICATION                                          DIVISION.
 
 
-       PROGRAM-ID. ProgCSV.
+       PROGRAM-ID. PROGCSV.
 
        ENVIRONMENT                                             DIVISION.
 
@@ -17,7 +17,8 @@
 
        INPUT-OUTPUT                                             SECTION.
        FILE-CONTROL.
-           SELECT ARQCSV ASSIGN TO 'C:\Users\gotic\Desktop\cobol'
+           SELECT ARQCSV ASSIGN 
+           TO 'C:\Users\gotic\opencobolfiles\bin\teste.csv'
                ORGANIZATION IS LINE SEQUENTIAL.
 
        DATA                                                    DIVISION.
@@ -31,41 +32,45 @@
        77 WS-NAME-F       PIC X(10).
        77 WS-NAME-L       PIC X(10).
        77 WS-EMAIL        PIC X(30).
-       77 WS-PHONE        PIC 9(14).
+       77 WS-PHONE        PIC x(14).
+       77 WS-TESTE        PIC 9(04).
 
 
 
        PROCEDURE                                               DIVISION.
        0001-MAIN.
            OPEN OUTPUT ARQCSV
-           
+
             DISPLAY
            "**********************************************************".
            MOVE 'CELSO'           TO WS-NAME-F.
            MOVE 'LIMA'            TO WS-NAME-L.
            MOVE 'CELSO@CELSO.COM' TO WS-EMAIL.
-           MOVE 980460205         TO WS-PHONE.
+           MOVE '5511980460200'   TO WS-PHONE.
+           MOVE WS-PHONE(10:)     TO WS-TESTE.
+      *    * 1234567890123
+      *    * 5511980460200
+           DISPLAY WS-TESTE.
 
-           
-
+   
            STRING FUNCTION TRIM(WS-NAME-F)      DELIMITED BY SIZE
                            ' '                  DELIMITED BY SIZE
                   FUNCTION TRIM(WS-NAME-L)      DELIMITED BY SIZE
                            ', '                 DELIMITED BY SIZE
                   FUNCTION TRIM(WS-EMAIL)       DELIMITED BY SIZE
                            ', '                 DELIMITED BY SIZE
-                           ' + '                DELIMITED BY SIZE
+                           '+'                  DELIMITED BY SIZE
                   FUNCTION TRIM(WS-PHONE(1:2))  DELIMITED BY SIZE
                            ' '                  DELIMITED BY SIZE
                            '('                  DELIMITED BY SIZE
-                  FUNCTION TRIM(WS-PHONE(3:4))  DELIMITED BY SIZE
+                  FUNCTION TRIM(WS-PHONE(3:2))  DELIMITED BY SIZE
                            ')'                  DELIMITED BY SIZE
                            ' '                  DELIMITED BY SIZE
-                  FUNCTION TRIM(WS-PHONE(5:5))  DELIMITED BY SIZE
+                  FUNCTION TRIM(WS-PHONE(5:1))  DELIMITED BY SIZE
                            '.'                  DELIMITED BY SIZE
-                  FUNCTION TRIM(WS-PHONE(6:9))  DELIMITED BY SIZE
-                           ' '                  DELIMITED BY SIZE
-                  FUNCTION TRIM(WS-PHONE(10:))  DELIMITED BY SIZE
+                  FUNCTION TRIM(WS-PHONE(6:4))  DELIMITED BY SIZE
+                           '.'                  DELIMITED BY SIZE
+                  FUNCTION TRIM(WS-PHONE(10:))  DELIMITED BY SIZE         
                   INTO REG-CSV
            END-STRING.
 
@@ -74,4 +79,4 @@
 
             STOP RUN.
        0001-MAIN-END.
-       END PROGRAM ProgCSV.
+       END PROGRAM PROGCSV.
