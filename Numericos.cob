e7196d0ed3f0eee17ec282e66b12acd831acb32d
Celso Lima <celsolog2012@gmail.com>
2022-12-07 12:42:36 -0300

    conceitos de variaveis numericos incompleto

--- Numericos.cob
+++ Numericos.cob
@@ -6,12 +6,19 @@
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. Numericos.
+       ENVIRONMENT                                             DIVISION.
+
+       CONFIGURATION                                            SECTION.
+       SPECIAL-NAMES.
+           DECIMAL-POINT IS COMMA.
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
-       77 WS-TM-1 PIC 99.
-       77 WS-TM-2 PIC 99.
-       
+       01 WS-VARIAVEIS.
+          03 WS-INT        PIC 9(09)     VALUE ZEROS.
+          03 WS-DEC        PIC 9(09)V99  VALUE ZEROS. 
+          03 WS-VLR        PIC S9(09)V99 VALUE ZEROS. 
+
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
            DISPLAY "Hello world".
@@ -22,18 +29,23 @@
                
        0100-INIT.
            DISPLAY "INICIO DO PROGRAMA...".
+           initialize WS-VARIAVEIS.
 
 
            
        0200-PROCESS.
            DISPLAY "PROCESSAMENTO DO PROGRAMA...".
-           MOVE ZEROES        TO WS-TM-1.
-           INSPECT FUNCTION REVERSE(WS-PRIMEIRO-NOME)
-               TALLYING WS-TM-1 FOR LEADING ' '.
-               
+
+           MOVE 1 TO WS-INT WS-DEC WS-VLR.
            
+           COMPUTE WS-INT = WS-INT / 2.
+           COMPUTE WS-DEC = WS-DEC / 2.
+           COMPUTE WS-VLR = (WS-VLR / 2) * -1 .
+
        0300-DISPLAY.
- 
+           display "WS-INT: " WS-INT
+           display "WS-DEC: " WS-DEC
+           display "WS-VLR: " WS-VLR.
 
        9999-FINAL.
            DISPLAY "FINAL DO PROGRAMA...".
